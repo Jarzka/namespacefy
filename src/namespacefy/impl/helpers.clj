6c13f6f1f32aeb14c3f94ced63fbd5620b2d1b94
Jari Hanhela <jarihanhela@gmail.com>
2017-04-21 21:54:21 +0300

    Change the order of the functions

--- src/namespacefy/impl/helpers.clj
+++ src/namespacefy/impl/helpers.clj
@@ -3,11 +3,22 @@
             [clojure.set :as set]))
 
 (declare namespacefy)
+(declare unnamespacefy)
 
 (defn namespacefy-keyword [namespace-as-keyword keyword-to-be-modified]
   (assert (keyword? namespace-as-keyword) "Namespace must be a keyword.")
   (assert (keyword? keyword-to-be-modified) "Can only namespacefy keywords.")
   (keyword (str (name namespace-as-keyword) "/" (name keyword-to-be-modified))))
+
+(defn unnamespacefy-keyword [keyword-to-be-modified]
+  (assert (keyword? keyword-to-be-modified) "Can only unnamespacefy keywords.")
+  (keyword (name keyword-to-be-modified)))
+
+(defn- validate-map-to-be-unnamespaced [map-x]
+  (let [all-keywords (set (keys map-x))
+        unnamespaced-keywords (set (map unnamespacefy-keyword all-keywords))]
+    (when (not= (count all-keywords) (count unnamespaced-keywords))
+      (throw (AssertionError. "Unnamespacing would result a map with more than one keyword with the same name.")))))
 
 (defn- namespacefy-map [map-x {:keys [ns except custom inner] :as options}]
   (let [except (or except #{})
@@ -31,18 +42,6 @@
 
         (vector? data)
         (mapv #(namespacefy-map % options) data)))
-
-(declare unnamespacefy)
-
-(defn unnamespacefy-keyword [keyword-to-be-modified]
-  (assert (keyword? keyword-to-be-modified) "Can only unnamespacefy keywords.")
-  (keyword (name keyword-to-be-modified)))
-
-(defn- validate-map-to-be-unnamespaced [map-x]
-  (let [all-keywords (set (keys map-x))
-        unnamespaced-keywords (set (map unnamespacefy-keyword all-keywords))]
-    (when (not= (count all-keywords) (count unnamespaced-keywords))
-      (throw (AssertionError. "Unnamespacing would result a map with more than one keyword with the same name.")))))
 
 (defn- unnamespacefy-map
   [map-x {:keys [except recur?] :as options}]
