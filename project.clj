94c5bae10e15e8e17bfec70ee83b144dcda0bcb0
Jari Hanhela <jarihanhela@gmail.com>
2017-04-21 23:40:44 +0300

    Add project url and license info to the project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "Helper library for namespacing keywords"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+  :url "https://github.com/Jarzka/namespacefy"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
   :test-paths ["test"])
