9f39e2e9177a9a675795493a29af12c78426521e
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-07 12:35:45 +0300

    Upgrade version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject namespacefy "0.3"
+(defproject namespacefy "0.4"
   :description "Helper library for namespacing keywords"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
